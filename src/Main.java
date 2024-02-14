5b5d53c2800924a12e8819d6bbd3f86f35015dfe
Александр <kmuken@bk.ru>
2024-02-14 20:19:10 +0500

    zdelal 1 zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // zadanie 1
+
+        int odin = 2000000;
+        byte dva = 100;
+        short tri = 30000;
+        long chetire = 900000000;
+        float piat = 35.2F;
+        double shest = 208.2;
+
+        System.out.println("Значениее переменной с типом int равно " + odin);
+        System.out.println("Значениее переменной с типом byte равно " + dva);
+        System.out.println("Значениее переменной с типом short равно " + tri);
+        System.out.println("Значениее переменной с типом long равно " + chetire);
+        System.out.println("Значениее переменной с типом float равно " + piat);
+        System.out.println("Значениее переменной с типом double равно " + shest);
     }
 }
